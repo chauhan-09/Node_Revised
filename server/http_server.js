5af74aa9b8e06b6b9b3ea533657d0668cb1541af
chauhan-09 <nsc7574@gmail.com>
2025-11-12 19:25:09 +0530

    made changes

--- server/http_server.js
+++ server/http_server.js
@@ -1,7 +1,7 @@
 const http = require('http');                    //http is an inbuilt module in node
-const server = http.createServer((req,res)=> {  //req , res are request object and response object
+const server = http.createServer((req,res) => {   //req , res are request object and response object
     res.write("server is running");
-    res.end();
+    res.end();  //have to end the connection otherwise client will wait for endtime
 })
 
 server.listen(3000, () => {
